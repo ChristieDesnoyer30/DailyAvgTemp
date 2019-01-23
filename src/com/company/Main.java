501cf8a107d92a60dab79183791c6d687576b3f9
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-01-23 11:10:02 -0500

    Adding logic to return avg weekly temperature and to print out each temperature individually

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        double sum= 0.0;
+        double avg = 0.0;
+        DecimalFormat df = new DecimalFormat("0.00");
 
         Scanner scan = new Scanner(System.in);
 
@@ -13,14 +17,29 @@ public class Main {
 
 	for(int i  = 0; i < dailyAverageTemp.length; i++){
 
-        System.out.println("Please enter the temperature for day " + (i+1));
+        System.out.println("Please enter the temperature for day " + (i + 1));
+
         double dailyTemp = scan.nextDouble();
+
         dailyAverageTemp[i] = dailyTemp;
+
+        sum += dailyAverageTemp[i];
 
     }
 
+	System.out.println("The daily temperatures are: ");
+	for (int i = 0; i <dailyAverageTemp.length; i++){
 
-    System.out.println("The temperatures are: " +  Arrays.toString(dailyAverageTemp));
+	    System.out.print("Day " + (i + 1) + ": " + dailyAverageTemp[i] + " ");
+
+    }
+
+	avg = (sum/dailyAverageTemp.length);
+
+    System.out.println("\nThe average of the temperatures is: " + df.format(avg));
+
+
+
 
 
     }
